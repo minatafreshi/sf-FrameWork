b9e5fece8610a0ac52aa27d4660a818780ba9cb5
mina tafreshi <info.minatafreshi@gmail.com>
2020-01-25 18:28:03 +0330

    change front.php

--- web/front.php
+++ web/front.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-use Symfony\Component\HttpKernel;
+//use Symfony\Component\HttpKernel;
 
 
 function render_template($request)
@@ -21,26 +21,14 @@ $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../src/app.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
 
-try {
-    $request->attributes->add($matcher->match($request->getPathInfo()));
+$frameWork = new simplex\Framework($matcher, $controllerResolver, $argumentResolver);
 
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-
-    $response = call_user_func_array($controller, $arguments);
-    
-} catch (Routing\Exception\ResourceNotFoundException $exception) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$response = $frameWork->handle($request);
 
 $response->send();
-
 ?>
